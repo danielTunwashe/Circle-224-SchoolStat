9a2d50a03be100cd66dc6865ecbbe9deaf42a848
De-marauder <65220956+de-marauder@users.noreply.github.com>
2022-07-28 17:34:39 +0000

    Wrote the javascript functions for the sort and hamburger

--- app.mjs
+++ app.mjs
@@ -5,6 +5,31 @@ function startApp() {
 
   // pls remove the below and make some magic in here!
 
+  sortTables();
+  hamburgerLogic();
+
+};
+
+function hamburgerLogic () {
+  let toggle = true;
+
+  const menuBtn = document.getElementById("hamburger");
+  const sidebar = document.getElementById("sidebar");
+
+  menuBtn.addEventListener('click', ()=>{
+    if (toggle) {
+      sidebar.style.transform = "translateX(0)"
+
+      toggle = !toggle;
+    } else {
+      sidebar.style.transform = "translateX(100%)"
+      toggle = !toggle;
+    }
+  });
+}
+
+function sortTables () {
+
   // This is a list of table ids. They will be used to get the different tables
   const tables = ['instructor-table', 'track-table', 'gender-table', 'stats-table']
 
@@ -12,7 +37,7 @@ function startApp() {
   const sortableBtns = document.querySelectorAll('.sortable');
 
   // Initialize a toggler. This will be used to switch between sorting in ascending or descending order
-  let instructorTableToggle = false;
+  let tableToggle = false;
 
   // Assign onclick handlers for all sortable buttons
   sortableBtns.forEach((el, id) => {
@@ -21,11 +46,11 @@ function startApp() {
 
   // Table sorting Function.
   // @params: table id (from HTML markup)
-  function sortTable(table) {
+  function sortTable(tableID) {
     // Get a table
-    const instructorTable = document.getElementById(table);
+    const table = document.getElementById(tableID);
     // Get the rows in the table
-    const instructorTableRows = instructorTable.querySelectorAll('.table-row');
+    const tableRows = table.querySelectorAll('.table-row');
 
     // ```
     // Instantiate an array to store the table rows.
@@ -33,35 +58,36 @@ function startApp() {
     // 'querySelectorAll' is of type 'NodeList' and does not 
     // come with the sort method which is a crucial part of this function
     // ```
-    const instructorTableRowsItemsList = [];
+    const tableRowsItemsList = [];
 
-    instructorTableRows.forEach((element, id) => {
-      const child = element.children;
+    tableRows.forEach((element, id) => {
+      // const child = element.children;
 
       if (id > 0) {
-        instructorTableRowsItemsList.push(element)
+        tableRowsItemsList.push(element)
       }
     })
 
-    for (let i = 1; i < instructorTableRows.length; i++) {
-      instructorTableRows[i].remove();
+    for (let i = 1; i < tableRows.length; i++) {
+      tableRows[i].remove();
     }
 
-    instructorTableRowsItemsList.sort((a, b) => {
-      return instructorTableToggle ?
+    tableRowsItemsList.sort((a, b) => {
+      return tableToggle ?
         (+(b.children[b.children.length - 1].textContent) - +(a.children[a.children.length - 1].textContent)) :
         (+(a.children[a.children.length - 1].textContent) - +(b.children[b.children.length - 1].textContent));
     })
 
 
-    for (let i = 0; i < instructorTableRowsItemsList.length; i++) {
-      instructorTable.appendChild(instructorTableRowsItemsList[i]);
+    for (let i = 0; i < tableRowsItemsList.length; i++) {
+      table.appendChild(tableRowsItemsList[i]);
     }
 
-    instructorTableToggle = !instructorTableToggle;
+    tableToggle = !tableToggle;
   }
+}
 
-};
+
 
 // ======= DO NOT EDIT ============== //
 export default startApp;
